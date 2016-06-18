c0d0b624d5719fd803913eddd75207d2b2623321
mahesh <mahesh0919@gmail.com>
2016-06-18 11:41:11 +0530

    Default MVC5 Demplate
    on top of Default MVC5 template we are going to add step by step code
    changes implemented in MVC 5

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5Demo.Startup))]
+namespace MVC5Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
