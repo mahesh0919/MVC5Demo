5386c56402dc7997f16053f49011db7cfa88b662
mahesh <mahesh0919@gmail.com>
2016-06-18 11:56:46 +0530

    Enable Google Authentication
    External authentication services configured.

--- App_Start/Startup.Auth.cs
+++ App_Start/Startup.Auth.cs
@@ -32,7 +32,7 @@ namespace MVC5Demo
             //   appId: "",
             //   appSecret: "");
 
-            //app.UseGoogleAuthentication();
+            app.UseGoogleAuthentication();
         }
     }
 }
